ffbceda2808ec8e7b55505f44779446fb58549b6
Michal <mastprint90@gmail.com>
2024-06-10 20:51:39 +0200

    generated project

---
+++ src/main/java/pl/sidmas/travelcrm/TravelcrmApplication.java
@@ -0,0 +1,13 @@
+package pl.sidmas.travelcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelcrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelcrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sidmas/travelcrm/TravelcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sidmas.travelcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelcrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
